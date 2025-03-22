502ec03744dc626073935c124dc6c2c3baeead84
SirPersimmon <atafeeva@icloud.com>
2025-03-22 20:11:39 +0300

    Update Constants.py

--- src/Constants.py
+++ src/Constants.py
@@ -1,6 +1,6 @@
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
-PATH = '/home/marcelo/openpose'
+PATH = '/kaggle/working/openpose'
 
 max_cosine_distance = 1
 nn_budget = None
@@ -8,4 +8,4 @@ nms_max_overlap = 1.0
 max_age = 100
 n_init = 20
 
-openpose_modelfolder =  "/home/marcelo/openpose/models/"
+openpose_modelfolder =  "/kaggle/working/openpose/models"
